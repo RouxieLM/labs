1608b9d979f46fe3a9810873bd79218a68110688
RouxieLM <lmathieu76@outlook.fr>
2025-05-11 20:07:34 +0200

    added logging

--- k8s_HA/start_vm.ps1
+++ k8s_HA/start_vm.ps1
@@ -1,5 +1,7 @@
 # Define working directory and VM names
-$loc = "F:\Repo\Vagrant"
+$loc = "F:\Repo\Vagrant\k8s_HA"
+$logFile = "$loc\vagrant-provisioning.log"
+
 $vmList = @(
   "master-1",
   "master-2",
@@ -10,8 +12,16 @@ $vmList = @(
   "loadbalancer"
 )
 
+# Clear previous log
+if (Test-Path $logFile) {
+  Remove-Item $logFile -Force
+}
+"[$(Get-Date -Format 'u')] Starting Vagrant VM launch..." | Out-File $logFile
+
 # --- Step 1: Start master-1 synchronously
 Write-Output "Starting master-1..."
+"[$(Get-Date -Format 'u')] Starting master-1..." | Out-File $logFile -Append
+
 $job = Start-Job -ScriptBlock {
     param($path, $vm)
     Set-Location $path
@@ -19,12 +29,15 @@ $job = Start-Job -ScriptBlock {
 } -ArgumentList $loc, $vmList[0]
 
 Wait-Job $job
-Receive-Job $job
+Receive-Job $job | Out-File $logFile -Append
 Remove-Job $job
+
 Write-Output "master-1 started."
+"[$(Get-Date -Format 'u')] master-1 started." | Out-File $logFile -Appen
 
 # --- Step 2: Start remaining VMs in parallel
 Write-Output "Starting remaining VMs in parallel..."
+"[$(Get-Date -Format 'u')] Starting remaining VMs in parallel..." | Out-File $logFile -Append
 $parallelJobs = @()
 
 foreach ($vm in $vmList[1..($vmList.Count -1)]) {
@@ -40,6 +53,7 @@ Wait-Job -Job $parallelJobs
 
 foreach ($job in $parallelJobs) {
   Write-Output "----- Output from job ID $($job.Id) -----"
-  Receive-Job $job
+  "[$(Get-Date -Format 'u')] ----- Output from job ID $($job.Id) -----" | Out-File $logFile -Append
+  Receive-Job $job | Out-File $logFile -Append
   Remove-Job $job
 }
